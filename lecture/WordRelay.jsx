224463290ba8abc612d5cc12f0f39984377f2aee
sungwooHa <ttlhi10@gmail.com>
2021-08-09 17:21:43 +0900

    react class -> hooks

--- lecture/WordRelay.jsx
+++ lecture/WordRelay.jsx
@@ -1,5 +1,5 @@
 const React = require("react");
-const { useState } = require("react");
+const { useState, userRef } = React;
 
 const WordRelay = () => {
   const [word, setWord] = useState("하성우");
@@ -8,37 +8,35 @@ const WordRelay = () => {
   const inputEle = React.useRef(null);
 
   const onSubmitForm = (e) =>  {
-    e.preventDefault();
-    //제시어의 마지막 글자가 입력어의 첫번째 글자와 같은지 확인
-    if(word[word.length - 1] == value[0]){
-        setResult("딩동댕");
-        setWord(value);
+      e.preventDefault();
+      //제시어의 마지막 글자가 입력어의 첫번째 글자와 같은지 확인
+      if(word[word.length - 1] == value[0]){
+          setResult("딩동댕");
+          setWord(value);
+          setValue("");
+          inputEle.current.focus();
+      } else{
+        setResult('땡');
         setValue("");
-    } else{
-      setResult('땡');
-      setValue("");
+        inputEle.current.focus();
     }
-    inputEle.current.focus();
   };
 
   const onChangeInput=(e)=>{
     setValue(e.target.value);
   };
 
-  const onRefInput = (c) =>{
-    inputEle = c;
-  };
-
-  render() (
+  return(
     <>
-    <div>{this.state.word}</div>
-    <form onSubmit={this.onSubmitForm}>
-      <input ref={this.onRefInput} value ={this.state.value} onChange={this.onChangeInput}/>
+    <div>{word}</div>
+    <form onSubmit={onSubmitForm}>
+      <label id="label" htmlFor="wordInput">글자를 입력하세요. </label>
+      <input id="wordInput" className="wordInput" ref={inputEle} value ={value} onChange={onChangeInput}/>
       <button>입력!</button>
     </form>
-    <div>{this.state.answer}</div>
+    <div>{result}</div>
     </>
-  )
+  );
 }
 
 module.exports = WordRelay;
